321312dcbbe35ee6dbca935084dd12f974deca69
wojciech <pushupek009@interia.eu>
2018-10-07 04:31:25 -0700

    3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("TEST!");
         System.out.println("Witaj!");
-
+//test czy dziala git
     }
 }
